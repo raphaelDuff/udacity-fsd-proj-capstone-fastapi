dd93ac9c8a3b0dce641b4c7222753fd3f7e40d41
raphaelDuff <raphaelprates@outlook.com>
2025-02-11 08:07:33 -0300

    Adding get movies/details endpoint

--- routers/movies.py
+++ routers/movies.py
@@ -14,6 +14,17 @@ router = APIRouter(
 def create_item(session: Session = Depends(get_db)):
     stmt_select_all_movies = select(Movie).order_by(Movie.id)
     movies = session.scalars(stmt_select_all_movies).all()
-    # if len(list_movies) == 0:
-    #     abort(404)
-    return [{"id": movie.id, "title": movie.title} for movie in movies]
+    if len(movies) == 0:
+        raise HTTPException(status_code=404, detail=f"No movies were found!")
+    return {"success": True, "movies": [movie.short() for movie in movies]}
+
+
+@router.get("/details/{movie_id}")
+def get_movie_detail(movie_id: int, session: Session = Depends(get_db)):
+    stmt_movie_by_id = select(Movie).where(Movie.id == movie_id)
+    selected_movie = session.scalars(stmt_movie_by_id).one_or_none()
+    if selected_movie is None:
+        raise HTTPException(
+            status_code=404, detail=f"Movie id={movie_id} was not found!"
+        )
+    return {"success": True, "movies": selected_movie.long()}
